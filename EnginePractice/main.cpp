dd1a7c2f496cee96ca337b9d70d0aff1a397fd59
FBarnes99 <36264879+FBarnes99@users.noreply.github.com>
2019-08-20 17:41:34 +0200

    Opengl and SDL setup

--- EnginePractice/main.cpp
+++ EnginePractice/main.cpp
@@ -1,6 +1,10 @@
 #include <iostream>
+#include <glad/glad.h>
+#include <GLFW\glfw3.h>
+#define SDL_MAIN_HANDLED 
+#include "SDL.h"
 
-int main(int argc, const char* argv[]) {
+int main(int argc, const char * argv[]) {
 
 	return 0;
 
